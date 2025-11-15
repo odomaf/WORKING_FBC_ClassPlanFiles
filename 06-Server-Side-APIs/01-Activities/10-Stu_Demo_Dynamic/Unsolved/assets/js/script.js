14fd4581f0b9aafa382feb46de13767aebcb9b44
Anne Odom <afodom207@gmail.com>
2025-11-15 13:27:33 -0600

    Update: Breakout room for lesson 10 work

--- 06-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/10-Stu_Demo_Dynamic/Unsolved/assets/js/script.js
@@ -1,12 +1,23 @@
 // Select DOM nodes
-const peopleContainer = document.getElementById('people');
-const fetchButton = document.getElementById('load-people');
+const peopleContainer = document.getElementById("people");
+const fetchButton = document.getElementById("load-people");
 
 function getPeople() {
   // 1) Define the request URL to get page 1 of "people" from SWAPI.
   // Docs: https://swapi.dev/
-  const requestUrl = 'https://swapi.dev/api/people/?page=1';
+  const requestUrl = "https://swapi.dev/api/people/?page=1";
 
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (let i = 0; i < data.results.length; i++) {
+        const person = data.results[i];
+        console.log(`Person: ${person.name}`);
+      }
+    });
   // 2) Make a fetch() request.
   //    - First .then(): return response.json()
   //    - Second .then(data): console.log(data) and loop data.results
@@ -22,7 +33,6 @@ function getPeople() {
   //      - Append with peopleContainer.append(child)
   //
   //  🔧 Write your code below this comment:
-
 }
 
-fetchButton.addEventListener('click', getPeople);
+fetchButton.addEventListener("click", getPeople);
