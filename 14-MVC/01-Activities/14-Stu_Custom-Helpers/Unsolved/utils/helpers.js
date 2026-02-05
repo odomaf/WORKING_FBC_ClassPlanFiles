ed699068b8715d1d00dd164bac4f25f6b750fd29
Anne Odom <afodom207@gmail.com>
2026-02-04 19:36:53 -0500

    MODIFY: Working files from in-clas projects

--- 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
+++ 14-MVC/01-Activities/14-Stu_Custom-Helpers/Unsolved/utils/helpers.js
@@ -3,7 +3,13 @@ module.exports = {
     return date.toLocaleTimeString();
   },
   /*
-   ? TODO: Create a custom helper 'format_date' that takes in a timestamp,
-   ? adds five years to the date, and formats it as M/D/YYYY
-  */
+  ? TODO: Create a custom helper 'format_date' that takes in a timestamp,
+  ? adds five years to the date, and formats it as M/D/YYYY
+ */
+  format_date: (date) => {
+    //console.log(date);
+    let endDate = new Date(date);
+    endDate.setFullYear(date.getFullYear() + 5);
+    return endDate.toLocaleDateString();
+  },
 };
